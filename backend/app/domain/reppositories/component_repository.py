ad5767e6f35cb5aa210196e2e7dc6e5d03d35c4b
masamune <kenjiii534@gmail.com>
2023-03-27 19:37:00 +0900

    / にアクセスした時にそれっぽいページを表示出来るようにした

---
+++ backend/app/domain/reppositories/component_repository.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Components:
+    component_path: str
+    document: str
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/infrastructure/api/templates')
 
 
 @app.route("/")
-def hello_world():
+def component_list():
     """/ にゲットリクエストが来た時の関数
 
     Returns:
         _type_: _description_
     """
-    return "<p>Hello, World!</p>"
+    components = [
+        {'name': 'Component1', 'description': 'This is a sample component.'},
+        {'name': 'Component2', 'description': 'This is another sample component.'}
+    ]
+    return render_template("component_form.html", components=components)
+
+
+@app.route('/register_component', methods=['POST'])
+def register_component():
+    """コンポーネントを保存する関数
+
+    Returns:
+        Response : cpmponent_listのページを再度表示する
+    """
+    # フォームから送信されたデータを取得
+    name = request.form.get('name')
+    description = request.form.get('description')
+    file = request.files.get('file')
+
+    # ファイルとその他のデータをデータベースに保存する処理を実装する（省略）
+
+    # コンポーネントのリストページにリダイレクト
+    return redirect(url_for('component_list'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=True)
